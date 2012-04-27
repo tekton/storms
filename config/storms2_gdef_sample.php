59e6947c9d43c0358e1ff2efe21f7fce6ecf8c13
Tyler Agee <tyler@pyroturtle.com>
2012-04-27 10:49:44 -0700

    Cleaning up configs

--- config/storms2_gdef_sample.php
+++ config/storms2_gdef_sample.php
@@ -1,11 +1,11 @@
 <?php
 	/*start global variables */
-	define('DB', '');           // Database name -- all lower case OR uppercase, not both
-	define('USER', '');         // MySQL username
-	define('PASSWORD', '');     // MySQL password
-	define('HOST', '');         // location of MySQL server
-	define('TBLAPREFIX', '');   // In order to maintain multiple installs
-	define('DBUG', 0);          //debug level, checks greater than or equal to
-        define('URI_BASE','/path/to/storms3'); //Needs to be set per install...
-        /*end global variables*/
+	define('DB', '');          		// Database name -- all lower case OR uppercase, not both
+	define('USER', '');             // MySQL username
+	define('PASSWORD', '');    		// MySQL password
+	define('HOST', '');        		// location of MySQL server
+	define('TBLAPREFIX', '');     	// In order to maintain multiple installs
+	define('DBUG', 0);				// debug level
+	define('URI_BASE','/storms');	//Needs to be set per install...
+	/*end global variables*/
 ?>
