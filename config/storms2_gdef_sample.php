1f6a612967fd0facd722d2b0001e9347296017bc
tekton <pyroturtle@gmail.com>
2012-06-02 15:22:15 -0700

    cleaing up things and getting ready for files support

--- config/storms2_gdef_sample.php
+++ config/storms2_gdef_sample.php
@@ -5,7 +5,7 @@
 	define('PASSWORD', '{PASSWORD}');    		// MySQL password
 	define('HOST', '{HOST}');        		// location of MySQL server
 	define('TBLAPREFIX', '{TBLAPREFIX}');     	// In order to maintain multiple installs
-	define('DBUG', {DBUG});				// debug level
+	define('DBUG', '{DBUG}');				// debug level
 	define('URI_BASE','{URI_BASE}');	//Needs to be set per install...
 	/*end global variables*/
 ?>
