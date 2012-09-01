1c36420902a179bed16277fbf6aa1e4cd1b5f9bc
Tyler Agee <tyler@pyroturtle.com>
2012-09-01 09:49:45 -0700

    updating the samples to check them out later

--- config/storms_gdef_sample.php
+++ config/storms_gdef_sample.php
@@ -1,11 +1,11 @@
 <?php
 	/*start global variables */
-	define('DB', '{DB}');          		// Database name -- all lower case OR uppercase, not both
-	define('USER', '{USER}');             // MySQL username
-	define('PASSWORD', '{PASSWORD}');    		// MySQL password
-	define('HOST', '{HOST}');        		// location of MySQL server
-	define('TBLAPREFIX', '{TBLAPREFIX}');     	// In order to maintain multiple installs
-	define('DBUG', '{DBUG}');				// debug level
-	define('URI_BASE','{URI_BASE}');	//Needs to be set per install...
+	define('DB', 'stormsdev');          		// Database name -- all lower case OR uppercase, not both
+	define('USER', 'root');             // MySQL username
+	define('PASSWORD', '');    		// MySQL password
+	define('HOST', 'localhost');        		// location of MySQL server
+	define('TBLAPREFIX', 'storms_');     	// In order to maintain multiple installs
+	define('DBUG', '3');				// debug level
+	define('URI_BASE','/storms');	//Needs to be set per install...in OSX the last slash needs to not exist
 	/*end global variables*/
 ?>
